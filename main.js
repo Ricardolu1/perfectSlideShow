6fc3f6dca787932aedd33cfb02e4a1d4bfa9bcec
ycy <568082704@qq.com>
2019-07-28 21:33:29 +0800

    多加几张

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ var $container=$('.container')
 $buttons.eq(0).on("click", function(e) {
   let index= $(e.currentTarget).index()
   let len=$buttons.length
-  if (current===3&&index===0) {
+  if (current===5&&index===0) {
     $slides.css({ transform: `translateX(${-(len+1)*400}px)` })
     .one('transitionend',function() {
       $slides.hide().offset()
@@ -23,19 +23,31 @@ $buttons.eq(0).on("click", function(e) {
     $slides.css({ transform: `translateX(${-(index+1)*400}px)` })
   }
   current =1
-  n=0
+  n=index
 })
 
 $buttons.eq(1).on("click", function(e) {
   let index= $(e.currentTarget).index()
   $slides.css({ transform: `translateX(${-(index+1)*400}px)`})
   current =2
-  n=1
+  n=index
 })
-
 $buttons.eq(2).on("click", function(e) {
   let index= $(e.currentTarget).index()
-  if (current===1&&index===2) {
+  $slides.css({ transform: `translateX(${-(index+1)*400}px)`})
+  current =3
+  n=index
+})
+$buttons.eq(3).on("click", function(e) {
+  let index= $(e.currentTarget).index()
+  $slides.css({ transform: `translateX(${-(index+1)*400}px)`})
+  current =4
+  n=index
+})
+
+$buttons.eq(4).on("click", function(e) {
+  let index= $(e.currentTarget).index()
+  if (current===1&&index===4) {
     $slides.css({ transform: 'translateX(0)' })
     .one('transitionend',function() {
       $slides.hide().offset()
@@ -45,17 +57,17 @@ $buttons.eq(2).on("click", function(e) {
   }else{
     $slides.css({ transform: `translateX(${-(index+1)*400}px)` })
   }
-  current =3
-  n=2
+  current =5
+  n=index
 })
 
 
 
 var n=0
-$buttons.eq(n%3).click()
+$buttons.eq(n%5).click()
 var timer =setInterval(() => {
   n+=1
-  $buttons.eq(n%3).click()
+  $buttons.eq(n%5).click()
 }, 1500);
 
 document.addEventListener('visibilitychange', function(e){
@@ -64,7 +76,7 @@ document.addEventListener('visibilitychange', function(e){
   }else{
     timer =setInterval(() => {
       n+=1
-      $buttons.eq(n%3).click()
+      $buttons.eq(n%5).click()
     }, 1500);
   }
 })
@@ -75,7 +87,7 @@ $container.on('mouseenter',function() {
 $container.on('mouseleave',function() {
   timer =setInterval(() => {
     n+=1
-    $buttons.eq(n%3).click()
+    $buttons.eq(n%5).click()
   }, 1500);
 })
 

f278a7f838bdca05d51f6aec6aae780c5225953a
ycy <568082704@qq.com>
2019-08-03 23:23:13 +0800

    重写优化轮播

--- main.js
+++ main.js
@@ -1,97 +1,100 @@
 let $buttons = $("#buttonsWrapper>button")
 let $slides = $("#slides")
-let current
-let $images=$("#slides>img")
-let $firstFake=$images.eq($images.length-1).clone(true)
-let $lastFake=$images.eq(0).clone(true)
-$slides.append($lastFake)//append要用在父元素里面
-$slides.prepend($firstFake)
-var $container=$('.container')
+let $images = $("#slides>img")
+let current = 0
+makeFakeSlides()
+bindEvents()
 
-
-$buttons.eq(0).on("click", function(e) {
-  let index= $(e.currentTarget).index()
-  let len=$buttons.length
-  if (current===5&&index===0) {
-    $slides.css({ transform: `translateX(${-(len+1)*400}px)` })
-    .one('transitionend',function() {
-      $slides.hide().offset()
-      $slides.css({ transform: `translateX(${-(index+1)*400}px)` })
-      .show()
-    })
-  }else{
-    $slides.css({ transform: `translateX(${-(index+1)*400}px)` })
+function bindEvents() {
+  $("#buttonsWrapper").on("click", "button", function(e) {
+    let $button = $(e.currentTarget)
+    let index = $button.index()
+    goToSlide(index)
+  })
+}
+//重要，一旦我们拥有了直接到达某个slide的能力们就可以做左右轮播了
+function goToSlide(index) {
+  let len = $buttons.length
+  if (current === len - 1 && index === 0) {
+    //说明是最后一个到第一个
+    $slides
+      .css({ transform: `translateX(${-(len + 1) * 400}px)` })
+      .one("transitionend", function() {
+        $slides.hide().offset()
+        $slides.css({ transform: `translateX(-400px)` }).show()
+      })
+    current = index
+  } else if (current === 0 && index === $buttons.length - 1) {
+    //第一个到最后一个
+    $slides
+      .css({ transform: `translateX(0px)` })
+      .one("transitionend", function() {
+        $slides.hide().offset()
+        $slides.css({ transform: `translateX(${-len * 400}px)` }).show()
+      })
+  } else {
+    $slides.css({ transform: `translateX(${-(index + 1) * 400}px)` })
+    current = index
   }
-  current =1
-  n=index
-})
-
-$buttons.eq(1).on("click", function(e) {
-  let index= $(e.currentTarget).index()
-  $slides.css({ transform: `translateX(${-(index+1)*400}px)`})
-  current =2
-  n=index
-})
-$buttons.eq(2).on("click", function(e) {
-  let index= $(e.currentTarget).index()
-  $slides.css({ transform: `translateX(${-(index+1)*400}px)`})
-  current =3
-  n=index
-})
-$buttons.eq(3).on("click", function(e) {
-  let index= $(e.currentTarget).index()
-  $slides.css({ transform: `translateX(${-(index+1)*400}px)`})
-  current =4
-  n=index
-})
-
-$buttons.eq(4).on("click", function(e) {
-  let index= $(e.currentTarget).index()
-  if (current===1&&index===4) {
-    $slides.css({ transform: 'translateX(0)' })
-    .one('transitionend',function() {
-      $slides.hide().offset()
-      $slides.css({ transform: `translateX(${-(index+1)*400}px)` })
-      .show()
-    })
-  }else{
-    $slides.css({ transform: `translateX(${-(index+1)*400}px)` })
-  }
-  current =5
-  n=index
-})
-
-
-
-var n=0
-$buttons.eq(n%5).click()
-var timer =setInterval(() => {
-  n+=1
-  $buttons.eq(n%5).click()
-}, 1500);
-
-document.addEventListener('visibilitychange', function(e){
-  if(document.hidden){
-    window.clearInterval(timer)
-  }else{
-    timer =setInterval(() => {
-      n+=1
-      $buttons.eq(n%5).click()
-    }, 1500);
-  }
-})
-
-$container.on('mouseenter',function() {
-  window.clearInterval(timer)
-})
-$container.on('mouseleave',function() {
-  timer =setInterval(() => {
-    n+=1
-    $buttons.eq(n%5).click()
-  }, 1500);
-})
+}
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+//   $buttons.eq(0).on('click',function() {
+//     if (current===2) {
+//       console.log('说明你是从最后一张到第一张')
+//       $slides.css({transform:'translateX(-1600px)'})
+//       .one('transitionend',function() {
+//         $slides.hide()
+//           .offset()
+//         $slides.css({transform:'translateX(-400px)'})
+//           .show()
+//       })
+//     }else{
+//       $slides.css({transform:'translateX(-400px)'})
+//     }
+//     current=0
+//   })
+
+//   $buttons.eq(1).on('click',function() {
+//     console.log(current)
+//     $slides.css({transform:'translateX(-800px)'})
+//     current=1
+//   })
+
+//   $buttons.eq(2).on('click',function() {
+//     if (current===0) {
+//       console.log('说明你是从第一张到最后一张')
+//       $slides.css({transform:'translateX(0px)'})
+//       .one('transitionend',function() {
+//         $slides.hide()
+//           .offset()
+//         $slides.css({transform:'translateX(-1200px)'})
+//           .show()
+//       })
+//     }else{
+//       $slides.css({transform:'translateX(-1200px)'})
+//     }
+//     current=2
+//   })
+//
+
+function makeFakeSlides() {
+  let $firstFake = $images.eq($images.length - 1).clone(true)
+  let $lastFake = $images.eq(0).clone(true)
+
+  $slides.append($lastFake) //append要用在父元素里面
+  $slides.prepend($firstFake)
+}
